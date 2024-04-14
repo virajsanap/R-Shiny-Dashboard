2d77d041ca7aa5559b7b5af0c9b44381d1e4db6d
Viraj Sanap <64410732+virajsanap@users.noreply.github.com>
2024-04-14 15:36:27 -0400

    Update histogram.R

--- histogram.R
+++ histogram.R
@@ -1,10 +1,4 @@
-####################################
-# Data Professor                   #
-# http://youtube.com/dataprofessor #
-# http://github.com/dataprofessor  #
-####################################
 
-# Modified from https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/
 
 library(shiny)
 data(airquality)
